4b3fce64d5e4532c563227db31dfa92854f9bef1
Zero_Xhj <xiaohongjun@bytedance.com>
2018-05-22 17:02:17 +0800

    first commit

---
+++ ModuleA/ModuleA.swift
@@ -0,0 +1,13 @@
+//
+//  ModuleA.swift
+//  ModuleA
+//
+//  Created by xiaohongjun on 2018/5/22.
+//  Copyright © 2018年 xiaohongjun. All rights reserved.
+//
+
+class ModuleA {
+    func addModuleA() {
+        print("add moduleA")
+    }
+}
